408a516ec30147def2424c01582080e5b9eef916
Gerardo Daniel Garcia Cerda <daniel654garcia@gmail.com>
2026-07-24 12:10:32 -0600

    fix: configurar ruta base (base path) para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://copalinc.com',
+	base: '/web.omnimind',
 	integrations: [mdx(), sitemap()],
 	server: {
 		host: "127.0.0.1",
